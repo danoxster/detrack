64a5b04245ad863f92a257df3c074a94685937f7
Dan Simmonds <danoxster@gmail.com>
2025-08-17 14:50:35 +1200

    feat(test): add clients.json as a test fixture

--- test/test_client_search.rb
+++ test/test_client_search.rb
@@ -2,7 +2,16 @@ require "minitest/autorun"
 require "client_search"
 
 class TestClientSearch < Minitest::Test
-  def test_english_hello
+
+  def setup
+    @client_json_path = File.join(__dir__, 'fixtures', 'clients.json')
+  end
+
+  def test_fixture_path_setup
+    assert File.exist?(@client_json_path), "Fixture file does not exist at #{@client_json_path}"
+  end
+
+  def test_hello
     assert_equal "Hello from ClientSearch!", ClientSearch.say_hello
   end
 
